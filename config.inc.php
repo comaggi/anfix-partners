e55d1fa6ba351f27e579301f3bef3126c2e2bed8
Carlos Molina <ozonosp@gmail.com>
2016-03-21 08:36:35 +0000

    config.inc.php editado a través de la web en Bitbucket

--- config.inc.php
+++ config.inc.php
@@ -6,6 +6,7 @@
     'applicationIdUrl' => array(
         '1' => 'https://apps.anfix.com/os/os/parc/',
         'E' => 'https://apps.anfix.com/facturapro-servicios/gestiona/servicios/'
+        '3' => 'https://apps.anfix.com/contapro/conta/'
     ),
     'requestTokenUrl' => 'http://apps.anfix.com/os/os/parc/partner/request_token',
     'accessTokenUrl' => 'http://apps.anfix.com/os/os/parc/partner/access_token',
