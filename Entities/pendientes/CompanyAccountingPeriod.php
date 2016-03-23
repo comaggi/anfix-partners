d3a8801f9275516688a4bac0599bdc5bfb54ffb8
Carlos Molina <ozonosp@gmail.com>
2016-03-23 08:54:53 +0000

    CompanyAccountingPeriod.php editado a través de la web en Bitbucket

--- Entities/pendientes/CompanyAccountingPeriod.php
+++ Entities/pendientes/CompanyAccountingPeriod.php
@@ -23,7 +23,7 @@ namespace Anfix;
 class CompanyAccountingPeriod extends BaseModel
 {
     protected $applicationId = '3';
-    protected $apiBaseUrl = 'http://apps.anfix.com/contapro/conta/company/accountingperiod/';
+    protected $apiUrlSufix = 'company/accountingperiod/';
     protected $update = true;
     protected $create = false;
     protected $delete = true;
