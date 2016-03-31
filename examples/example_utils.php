a324226551c423c0ff5e59ff935b97adcfccd628
Carlos Molina <ozonosp@gmail.com>
2016-03-31 14:22:30 +0000

    example_utils.php editado a través de la web en Bitbucket

--- examples/example_utils.php
+++ examples/example_utils.php
@@ -5,3 +5,9 @@ function print_result($title,$value){
 	echo "<div style='color: #6d6d6d; background-color: #eee; font-family: Arial; padding: 5px 0 5px 10px; margin: 5px 0 15px;'>
 	<b style='color:#007acc;'>$title</b><pre>".print_r($value,true).'</pre></div>';
 }  
+
+//Devuelve el compoanyId de la primera empresa disponible
+function companyId(){
+	static $companyId = Anfix\Company::firstOrFail([])->CompanyId;
+	return $companyId;
+}
