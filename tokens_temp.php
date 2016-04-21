038ed92100dc2059f7c3492fa7f9e6bde0782ef1
Carlos Mora <ozonosp@gmail.com>
2016-04-21 19:36:52 +0200

    .

--- tokens_temp.php
+++ tokens_temp.php
@@ -1,8 +1,3 @@
 <?php return array (
-  '2D05AE163A18D090ABACD0A7DFB8DBE8' => 
-  array (
-    'identifier' => 'test_identifier',
-    'secret' => 'BDEB6227927C48A76AB62867B4B5A6A7',
-    'timestamp' => 1461169034,
-  ),
+
 );
